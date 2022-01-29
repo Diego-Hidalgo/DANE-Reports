5b6704459fd863f65b3a0a7a3e58d17420adb783
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2022-01-29 00:10:20 -0500

    Added StateData class

---
+++ model/StateData.cs
@@ -0,0 +1,34 @@
+﻿namespace DANE_Reports.model
+{
+    internal class StateData
+    {
+
+        private string _name;
+        private int _count;
+
+        internal StateData()
+        {
+            _name = default;
+            _count = default;
+        }
+
+        internal StateData(string Name, int Count)
+        {
+            this._name = Name;
+            this._count = Count;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public int Count
+        {
+            get => _count;
+            set => _count = value;
+        }
+
+    }
+}
