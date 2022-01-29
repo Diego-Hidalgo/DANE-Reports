e609f9580ad71ecdad774794bc73b94ee59dd63d
Diego Hidalgo Lopez <d13g0fhl@gmail.com>
2022-01-29 00:28:38 -0500

    Added methods to import the file data in DANEData class

--- model/DANEData.cs
+++ model/DANEData.cs
@@ -1,23 +1,71 @@
 ﻿using System.Collections.Generic;
+using System.IO;
 
 namespace DANE_Reports.model
 {
     internal class DANEData
     {
 
-        private List<Data> data;
+        private List<Data> _data;
+        private List<StateData> _states;
 
         public DANEData()
         {
-            this.data = new List<Data>();
+            this._data = new List<Data>();
         }
 
-        public List<Data> Entries
+        public List<Data> Data
         {
-            get => data;
-            set => data = value;
+            get => _data;
+            set => _data = value;
         }
 
+        public List <StateData> States
+        {
+            get => _states;
+            set => _states = value;
+        }
+        public void ImportFileData(string FilePath, char separator, bool header)
+        {
+            StreamReader sr = new StreamReader(FilePath);
+            if (header)
+                sr.ReadLine();
+            string line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                string[] parts = line.Split(separator);
+                if (parts.Length == 5)
+                {
+                    AddCity(parts[0], parts[1], parts[2], parts[3], parts[4]);
+                }
+            }
+        }
+
+        private void AddCity(string sCode, string cCode, string sName, string cName, string cType)
+        {
+            AddStateName(sName);
+            int i = 0;
+            while (i < _data.Count && _data[i].CityId.CompareTo(cCode) < 0)
+                ++i;
+            _data.Insert(i, new Data(sCode, cCode, sName, cName, cType));
+        }
+
+        private void AddStateName(string sName)
+        {
+            for (int i = 0; i < _states.Count; i++)
+            {
+                StateData current = _states[i];
+                if (current.Name == sName)
+                {
+                    current.Count++;
+                    return;
+                }
+            }
+            int j = 0;
+            while (j < _states.Count && _states[j].Name.CompareTo(sName) < 0)
+                ++j;
+            _states.Insert(j, new StateData(sName, 1));
+        }
 
     }
 }
--- model/Data.cs
+++ model/Data.cs
@@ -3,34 +3,28 @@
     internal class Data
     {
 
-        private string _stateName;
         private string _stateId;
-        private string _cityName;
         private string _cityId;
+        private string _stateName;
+        private string _cityName;
         private string _cityType;
 
         internal Data()
         {
-            _stateName = default;
             _stateId = default;
-            _cityName = default;
             _cityId = default;
+            _stateName = default;
+            _cityName = default;
             _cityType = default;
         }
 
-        internal Data(string StateName, string StateId, string CityName, string CityId, string CityType)
+        internal Data(string StateId, string CityId, string StateName, string CityName, string CityType)
         {
-            this._stateName = StateName;
             this._stateId = StateId;
-            this._cityName = CityName;
             this._cityId = CityId;
+            this._stateName = StateName;
+            this._cityName = CityName;
             this._cityType = CityType;
-        }
-
-        public string StateName
-        {
-            get => _stateName;
-            set => _stateName = value;
         }
 
         public string StateId
@@ -39,16 +33,22 @@
             set => _stateId = value;
         }
 
-        public string CityName
-        {
-            get => _cityName;
-            set => _cityName = value;
-        }
-
         public string CityId
         {
             get => _cityId;
             set => _cityId = value;
+        }
+
+        public string StateName
+        {
+            get => _stateName;
+            set => _stateName = value;
+        }
+
+        public string CityName
+        {
+            get => _cityName;
+            set => _cityName = value;
         }
 
         public string CityType
